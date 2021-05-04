1820899062279790194a83f0f7a82a03f2d1fb87
Dawid Rolinski <249844@student.pwr.edu.pl>
2021-05-04 22:59:15 +0200

    komilacja błedna

--- F0030B_lab8_zadanie_1_249844.c
+++ F0030B_lab8_zadanie_1_249844.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <pthread.h> 
+#include <semaphore.h> 
 #include <time.h>
-#include <pthread.h>
-#include <semaphore.h>
+#include <unistd.h>
 
-#define N 5
-#define THINNING 2
-#define HUNGRY 1
-#define EATTING 0
-#define LEFT (phnum + 4) % N
-#define RIGHT (phum + 1) % N
-
-int state[N];
-int phil[N] = {0 ,1 ,2 ,3 ,4};
-
-sem_t mutex;
-
-sem_t S[N];
+#define N 5 
+#define THINKING 2 
+#define HUNGRY 1 
+#define EATING 0 
+#define LEFT (phnum + 4) % N 
+#define RIGHT (phnum + 1) % N 
+  
+int state[N]; 
+int phil[N] = { 0, 1, 2, 3, 4 }; 
+  
+sem_t mutex; 
+sem_t S[N]; 
 
 void test(int phnum) 
 { 
     if (state[phnum] == HUNGRY 
         && state[LEFT] != EATING 
         && state[RIGHT] != EATING) { 
-        // stwierdza ze je 
+        // state that eating 
         state[phnum] = EATING; 
   
         sleep(2); 
@@ -75,4 +75,46 @@ void put_fork(int phnum)
     test(RIGHT); 
   
     sem_post(&mutex); 
+} void* philospher(void* num) 
+{ 
+  
+    while (1) { 
+  
+        int* i = num; 
+  
+        sleep(1); 
+  
+        take_fork(*i); 
+  
+        sleep(0); 
+  
+        put_fork(*i); 
+    } 
+} 
+  
+int main() 
+{ 
+  
+    int i; 
+    pthread_t thread_id[N]; 
+  
+    // semafory
+    sem_init(&mutex, 0, 1); 
+  
+    for (i = 0; i < N; i++) 
+  
+        sem_init(&S[i], 0, 0); 
+  
+    for (i = 0; i < N; i++) { 
+  
+        // tworzy process filozofow
+        pthread_create(&thread_id[i], NULL, 
+                       philospher, &phil[i]); 
+  
+        printf("Philosopher %d is thinking\n", i + 1); 
+    } 
+  
+    for (i = 0; i < N; i++) 
+  
+        pthread_join(thread_id[i], NULL); 
 } 
