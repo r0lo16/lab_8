ffd6c1c63b941c889ce8a5214fd5e9344048a332
Dawid Rolinski <249844@student.pwr.edu.pl>
2021-05-04 21:59:05 +0200

    void oraz filozofowie

--- F0030B_lab8_zadanie_1_249844.c
+++ F0030B_lab8_zadanie_1_249844.c
@@ -17,4 +17,25 @@ sem_t mutex;
 
 sem_t S[N];
 
-
+void test(int phnum) 
+{ 
+    if (state[phnum] == HUNGRY 
+        && state[LEFT] != EATING 
+        && state[RIGHT] != EATING) { 
+        // stwierdza ze je 
+        state[phnum] = EATING; 
+  
+        sleep(2); 
+  
+        printf("Philosopher %d takes fork %d and %d\n", 
+                      phnum + 1, LEFT + 1, phnum + 1); 
+  
+        printf("Philosopher %d is Eating\n", phnum + 1); 
+  
+        // sem_post(&S[phnum]) brak efektu
+        // podczas pracy
+        // tworzy głod filozofów
+        // podczas wykonywania putfork 
+        sem_post(&S[phnum]); 
+    } 
+} 
